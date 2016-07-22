1732abbb101cef656967e7d5b97f2625991cfb79
Lef.t <luis.fiorotto@hotmail.com>
2016-07-22 17:38:07 -0300

    Commit inicial.
    Contiene todos los archivos base, a partir de los cuales se trabaja el
    sitio.

---
+++ js/contact_me.js
@@ -0,0 +1,75 @@
+// Contact Form Scripts
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+$(function () {
+    "use strict";
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val(),
+                email = $("input#email").val(),
+            //var phone = $("input#phone").val(),
+                message = $("textarea#message").val(),
+                firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "././mail/contact_me.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    //phone: phone,
+                    email: email,
+                    message: message
+                },
+                cache: false,
+                success: function () {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Su mensaje fue enviado. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function () {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Disculpas " + firstName + ", parece que el servidor de correo electrónico no funciona en este momento, intenta nuevamente más tarde!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                }
+            });
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        }
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function () {
+    "use strict";
+    $('#success').html('');
+});
